03b30f0c7cc2f30c826e967f0d4d5679faf2ef50
matjias <sixped@gmail.com>
2023-01-31 13:53:30 -0600

    added links to download of NOH report

---
+++ pages/documents/noh.tsx
@@ -0,0 +1,15 @@
+import styles from './noh.module.css'
+
+
+const Documents = () => {
+    return (
+        <div className={styles.wrapper}>
+            <h3>NOH x NordInsight</h3>
+            <p>Afsluttende rapport vedrørende test forløb udført i efteråret 2022 kan findes her:</p>
+            <a href='http://files.nordinsight.com/documents/Afsluttende_rapport_NOH_NordInsight_2022.pdf' target={'_blank'} rel="noreferrer">PDF</a>
+            <a href='http://files.nordinsight.com/documents/Afsluttende_rapport_NOH_NordInsight_2022.docx'>WORD</a>
+        </div>
+    )
+}
+
+export default Documents;
