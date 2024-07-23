eb2ea341aeab29510dfc515a827b9a5a72aa5caf
Mathias Sixten <sixped@gmail.com>
2024-07-23 16:03:00 +0200

    geo ip country log

--- pages/api/log/search.ts
+++ pages/api/log/search.ts
@@ -1,7 +1,7 @@
 import { config, sendMail } from '../../../lib/mail'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import requestIp from 'request-ip'
-import geoip from 'geoip-country'
+import geoip from 'geoip-lite'
 
 type Data = {
   status: number
@@ -17,17 +17,21 @@ export default async function handler(
     const detectedIp = requestIp.getClientIp(req)
 
     console.log(detectedIp)
-    // const fromCountry = geoip.lookup(detectedIp)
+    const geo = geoip.lookup(detectedIp)
     const searchTerm = req.body.searchTerm
-    // console.log('Search term: ' + searchTerm + '\n' + 'From: ' + fromCountry)
-    const fromCountry = detectedIp
+
+    const geoString = geo
+      ? `Country: ${geo.country ? geo.country : detectedIp}, Region: ${
+          geo.region ? geo.region : detectedIp
+        }, City: ${geo.city ? geo.city : detectedIp}`
+      : detectedIp
 
     const mailData = {
       sender: 'NordInsight',
       senderMail: 'form@nordinsight.com',
       recipients: ['msixten@nordinsight.com'],
       subject: `There has been a search on nordinsight.com`,
-      body: 'Search term: ' + searchTerm + '\n' + 'From: ' + fromCountry,
+      body: 'Search term: ' + searchTerm + '\n' + 'From: ' + geoString,
     }
     const info = await sendMail(mailData)
   }
