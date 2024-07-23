6e2aee83832568fda605fc7bdfb873266e2fe69a
Mathias Sixten <sixped@gmail.com>
2024-07-23 16:23:50 +0200

    disabling geo lookup for now

--- pages/api/log/search.ts
+++ pages/api/log/search.ts
@@ -14,24 +14,24 @@ export default async function handler(
   if (req.method === 'POST') {
     config()
 
+    const searchTerm = req.body.searchTerm
     const detectedIp = requestIp.getClientIp(req)
 
-    console.log(detectedIp)
-    const geo = geoip.lookup(detectedIp)
-    const searchTerm = req.body.searchTerm
+    // console.log(detectedIp)
+    // const geo = geoip.lookup(detectedIp)
 
-    const geoString = geo
-      ? `Country: ${geo.country ? geo.country : detectedIp}, Region: ${
-          geo.region ? geo.region : detectedIp
-        }, City: ${geo.city ? geo.city : detectedIp}`
-      : detectedIp
+    // const geoString = geo
+    //   ? `Country: ${geo.country ? geo.country : detectedIp}, Region: ${
+    //       geo.region ? geo.region : detectedIp
+    //     }, City: ${geo.city ? geo.city : detectedIp}`
+    //   : detectedIp
 
     const mailData = {
       sender: 'NordInsight',
       senderMail: 'form@nordinsight.com',
       recipients: ['msixten@nordinsight.com'],
       subject: `There has been a search on nordinsight.com`,
-      body: 'Search term: ' + searchTerm + '\n' + 'From: ' + geoString,
+      body: 'Search term: ' + searchTerm + '\n' + 'From: ' + detectedIp,
     }
     const info = await sendMail(mailData)
   }
